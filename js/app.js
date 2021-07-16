2c43ee43e4fd9b342701ba636200d3f485c6a798
Christian-Chowaniec <christian.chowaniec03@gmail.com>
2021-07-16 22:42:04 +0200

    update 2

--- js/app.js
+++ js/app.js
@@ -1,30 +1,18 @@
 const inputProducts = document.querySelector("#products")
 const inputOrders = document.querySelector("#orders")
-
-const selectInputID = document.querySelector("#package")
-const calcInput = document.querySelector(".calc__input")
-
-const selectInput = document.querySelector(".select__input")
-const selectDropdown = document.querySelector(".select__dropdown")
-const itemDropdown = document.querySelectorAll(".item__dropdown")
 const itemList = document.querySelectorAll(".list__item")
 const namePackage = document.querySelectorAll(".item__calc")
 const pricePackage = document.querySelectorAll(".item__price")
 
+const selectInput = document.querySelector(".select__input")
+const selectDropdown = document.querySelector(".select__dropdown")
+const itemDropdown = document.querySelectorAll(".item__dropdown")
+
 const checkboxAccounting = document.querySelector("#accounting")
 const checkboxTerminal = document.querySelector("#terminal")
 
-// -------- do wyrzucenia
-// const listProducts = document.querySelector("#products")  // tego nie moze byc, bedzie sie gryzło
-// const listOrders = document.querySelector("#orders") // tego nie moze byc, bedzie sie gryzło
-// const listPackage = document.querySelector("#package") // tego nie moze byc, bedzie sie gryzło
-// const listAccounting = document.querySelector("#accounting") // tego nie moze byc, bedzie sie gryzło
-
-// const totalPriceSummaryClass = document.querySelector("#summary__total")
 const totalPriceSummary = document.querySelector("#total-price")
 const totalPrice = document.querySelector(".total__price")
-
-
 
 let itemPrices = {
     priceQuantity: 0,
@@ -33,25 +21,47 @@ let itemPrices = {
     priceTerminal: 0,
     priceAccounting: 0,
 }
-
-
-
 // QUANTITY
-inputProducts.addEventListener("input", function(){
+inputProducts.addEventListener("input", function () {
     itemList[0].style.display = "flex"
+    inputProducts.style.color = "black"
+    let quantity = inputProducts.value
+    let quantitySummary = inputProducts.value * 3
+    let replaceNamePackage = document.querySelector(".item__calc")
+    let pricePackage = document.querySelector(".item__price")
+
+    if (inputProducts.value > 0) {
+        replaceNamePackage.innerText = quantity + " * " + "$3"
+        pricePackage.innerText = "$" + quantitySummary
+    } else {
+        replaceNamePackage.innerText = "error"
+        pricePackage.innerText = "$" + 0
+        quantitySummary = 0
+    }
+    itemPrices.priceQuantity = quantitySummary
+    allSum()
 })
-
-
-
 // ORDERS
-inputOrders.addEventListener("input", function(){
+inputOrders.addEventListener("input", function () {
     itemList[1].style.display = "flex"
+    inputOrders.style.color = "black"
+    let quantity = inputOrders.value
+    let quantitySummary = inputOrders.value * 0.5
+    let replaceNamePackage = document.querySelectorAll(".item__calc")
+    let pricePackage = document.querySelectorAll(".item__price")
+
+    if (inputOrders.value > 0) {
+        replaceNamePackage[1].innerText = quantity + " * " + "$0.5"
+        pricePackage[1].innerText = "$" + quantitySummary
+    } else {
+        replaceNamePackage[1].innerText = "error"
+        pricePackage[1].innerText = "$" + 0
+        quantitySummary = 0
+    }
+    itemPrices.priceOrder = quantitySummary
+    allSum()
 })
-
-
-
 // CHOOSE PACKAGE
-
 selectInput.addEventListener("click", function () {
     selectDropdown.style.display = "block"
     selectInput.style.boxShadow = "0 0 10px rgb(8, 166, 228)"
@@ -60,106 +70,67 @@ selectDropdown.addEventListener("click", function () {
     selectDropdown.style.display = "none"
     selectInput.style.boxShadow = ""
 })
+itemDropdown[0].addEventListener("click", (infoBasic))
+itemDropdown[1].addEventListener("click", (infoProfessional))
+itemDropdown[2].addEventListener("click", (infoPremium))
 
-itemDropdown[0].addEventListener("click",(InfoBasic))
-itemDropdown[0].addEventListener("click",(allSum))
-
-itemDropdown[1].addEventListener("click",(InfoProfessional))
-itemDropdown[1].addEventListener("click",(allSum))
-
-itemDropdown[2].addEventListener("click",(InfoPremium))
-itemDropdown[2].addEventListener("click",(allSum))
-
-function InfoBasic() {
-    let price = 5
+function infoBasic() {
+    let price = 0
     selectInput.innerText = itemDropdown[0].innerText
     namePackage[2].innerText = "Basic"
     pricePackage[2].innerText = "$" + price
     itemPrices.pricePackage = price
+    itemList[2].style.display = "flex"
+    selectInput.style.color = "black"
+    allSum()
 }
-function InfoProfessional() {
-    let price = 10
+
+function infoProfessional() {
+    let price = 25
     selectInput.innerText = itemDropdown[1].innerText
     namePackage[2].innerText = "Professional"
     pricePackage[2].innerText = "$" + price
     itemPrices.pricePackage = price
+    itemList[2].style.display = "flex"
+    selectInput.style.color = "black"
+    allSum()
 }
-function InfoPremium() {
-    let price = 15
+
+function infoPremium() {
+    let price = 60
     selectInput.innerText = itemDropdown[2].innerText
     namePackage[2].innerText = "Premium"
     pricePackage[2].innerText = "$" + price
     itemPrices.pricePackage = price
-}
-function allSum() {
     itemList[2].style.display = "flex"
     selectInput.style.color = "black"
-    totalPrice.innerText =  "$" + itemPrices.priceQuantity + itemPrices.priceOrder + itemPrices.pricePackage + itemPrices.priceTerminal + itemPrices.priceAccounting
+    allSum()
 }
-
-// -------- do wyrzucenia
-// itemDropdown[0].addEventListener("click", function () {
-//     let price = 0
-//     selectInput.innerText = itemDropdown[0].innerText
-//     itemList[2].style.display = "flex"
-//     namePackage[2].innerText = "Basic"
-//     selectInput.style.color = "black"
-//     pricePackage[2].innerText = "$" + price
-//     itemPrices.pricePackage = price
-//     totalPrice.innerText =  "$" + itemPrices.priceQuantity + itemPrices.priceOrder + itemPrices.pricePackage + itemPrices.priceTerminal + itemPrices.priceAccounting
-// })
-// itemDropdown[1].addEventListener("click", function () {
-//     let price = 10
-//     selectInput.innerText = itemDropdown[1].innerText
-//     itemList[2].style.display = "flex"
-//     namePackage[2].innerText = "Professional"
-//     selectInput.style.color = "black"
-//     pricePackage[2].innerText = "$" + price
-//     itemPrices.pricePackage = price
-//     totalPrice.innerText =  "$" + itemPrices.priceQuantity + itemPrices.priceOrder + itemPrices.pricePackage + itemPrices.priceTerminal + itemPrices.priceAccounting
-// })
-// itemDropdown[2].addEventListener("click", function () {
-//     let price = 15
-//     selectInput.innerText = itemDropdown[2].innerText
-//     itemList[2].style.display = "flex"
-//     namePackage[2].innerText = "Premium"
-//     selectInput.style.color = "black"
-//     pricePackage[2].innerText = "$" + price
-//     itemPrices.pricePackage = price
-//     totalPrice.innerText =  "$" + itemPrices.priceQuantity + itemPrices.priceOrder + itemPrices.pricePackage + itemPrices.priceTerminal + itemPrices.priceAccounting
-// })
-
-
-
 // CHECKBOX
 checkboxAccounting.addEventListener("click", function () {
-    if (checkboxAccounting.checked){
+    if (checkboxAccounting.checked) {
         itemList[3].style.display = "flex"
-        // price.accountingPrice = 5
-    console.log("on.checkboxAccounting")}
-    else {
+        itemPrices.priceAccounting = 5
+    } else {
         itemList[3].style.display = "none"
-        // price.accountingPrice = 0
-        console.log("off.checkboxAccounting")
+        itemPrices.priceAccounting = 0
     }
+    allSum()
 })
 checkboxTerminal.addEventListener("click", function () {
-    if (checkboxTerminal.checked){
+    if (checkboxTerminal.checked) {
         itemList[4].style.display = "flex"
-        // price.terminalPrice = 7
-        console.log("on.checkboxTerminal")}
-    else {
+        itemPrices.priceTerminal = 2
+    } else {
         itemList[4].style.display = "none"
-        // price.terminalPrice = 0
-        console.log("off.checkboxTerminal")
-
+        itemPrices.priceTerminal = 0
     }
+    allSum()
 })
-
-
-
 //TOTAL SUMMARY
 totalPriceSummary.style.display = "flex";
 
-// zostało do zrobienia dodawanie produktów,ilosc,checkboxy i wybranie pakietu
-//to be continued
+function allSum() {
+    totalPrice.innerText = "$" + (itemPrices.priceQuantity + +itemPrices.priceOrder + +itemPrices.pricePackage + +itemPrices.priceTerminal + +itemPrices.priceAccounting)
+    console.log(totalPrice.innerText)
+}
